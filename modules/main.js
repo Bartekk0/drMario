eeb1054c081245f2db443f45407b35c32468cc93
Bartek <bwatorek86@uczen.tl.krakow.pl>
2024-10-06 22:51:28 +0200

    Added choosing speed

--- modules/main.js
+++ modules/main.js
@@ -1,6 +1,26 @@
 import Game from "./game.js";
 
-const newGame = new Game(400);
+const speeds = {
+    low: 600,
+    med: 400,
+    hi: 300,
+};
+
+const ls = localStorage.getItem("MarioSpeed");
+console.log(speeds[ls]);
+
+let speed = speeds["low"];
+if (ls) {
+    speed = speeds[ls];
+} else {
+    speed = speeds["low"];
+    localStorage.setItem("MarioSpeed", "low");
+}
+
+console.log(speed);
+
+const newGame = new Game(speed);
 newGame.start();
 
 export default newGame;
+
